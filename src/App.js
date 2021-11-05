b40db4103543ee4c817e26b930568f3ee2d07993
astephens-fsd <astephens.pm@icloud.com>
2021-11-05 13:41:13 -0700

    created character function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import React, {useState, useEffect} from 'react';
+// import './App.css';
+// import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  // const [data, setData]
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+
+import React from "react";
+//import styled from 'styled-components'
+
+export default function Character(props){
+    console.log(props)
+
+    return(
+        <div className='Character'>
+            <h2>Star Wars Characters</h2>
+            <p>Name: {props.data.name}</p>
+            <p>Height: {props.data.height}</p>
+            <p>Eye Color: {props.data.eye_color}</p>
+            <p>Hair Color: {props.data.hair_color}</p>
+            <p>Birth Year: {props.data.birth_year}</p>
+            <p>Gender: {props.data.gender}</p>
+            <p>Homeworld: {props.data.homeworld}</p>
+            <p>Species: {props.data.species}</p>
+
+        </div>
+    )
+}
