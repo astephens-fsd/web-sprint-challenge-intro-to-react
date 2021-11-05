e71f7db368416b9b9ca2221119dfb6196a4bf756
astephens-fsd <astephens.pm@icloud.com>
2021-11-05 15:58:45 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-// import React from 'react';
-// import React, {useState, useEffect} from 'react';
-// import './App.css';
-// import axios from 'axios';
+
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  // const [data, setData]
+  const [data, setDetails] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+      console.log(response);
+      setDetails(response.data.results)
+    })
+    .catch(err => console.log(err));
+  },[])
+
+  console.log(data);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+        {data.map((char, index)=>
+        <Character key={char.url} data={data[index]}/>
+        )}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,13 @@
 // Write your Character component here
 
 import React from "react";
-//import styled from 'styled-components'
+// import styled from 'styled-components'
 
-export default function Character(props){
+function Character(props){
     console.log(props)
 
     return(
         <div className='Character'>
-            <h2>Star Wars Characters</h2>
             <p>Name: {props.data.name}</p>
             <p>Height: {props.data.height}</p>
             <p>Eye Color: {props.data.eye_color}</p>
@@ -17,7 +16,7 @@ export default function Character(props){
             <p>Gender: {props.data.gender}</p>
             <p>Homeworld: {props.data.homeworld}</p>
             <p>Species: {props.data.species}</p>
-
         </div>
     )
 }
+export default Character;
